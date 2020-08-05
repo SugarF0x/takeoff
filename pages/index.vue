300631f967e6f8dcc1209552593ab83b7db3622b
Sugar_F0x <malion.torchwood@yandex.ru>
2020-08-05 18:29:48 +0300

    :white_check_mark: Add friends display

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,35 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+  <v-layout row>
+    <v-row justify="center">
+      <v-col col="12"
+             md="5"
+             lg="4"
+             xl="3"
+
+             v-for="user in users"
+             :key="user.id"
+      >
+        <v-card class="ma-3"
+        >
+          <v-img
+            height="200px"
+            :src="user.avatar"
+          />
+          <v-card-title>{{ user.full_name }}</v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio repudiandae sint voluptate? Aperiam consequatur culpa cupiditate illum repudiandae saepe voluptas.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+  export default {
+    async asyncData(context) {
+      let response = await context.app.$axios.$get("/api/auth/index");
+      return { users: response }
+    }
+  };
 </script>
