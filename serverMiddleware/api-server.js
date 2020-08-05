a99b618fc828a626af670f928ec704cf50237dca
Sugar_F0x <malion.torchwood@yandex.ru>
2020-08-05 18:58:53 +0300

    :white_check_mark: Protect server from unresolved API calls

--- serverMiddleware/api-server.js
+++ serverMiddleware/api-server.js
@@ -1,9 +1,16 @@
 export default async (req, res, next) => {
   let url = req._parsedUrl.pathname.replace(/^\/+|\/+$|\.+/g, "");
   url = url.split("/");
-  let method = url.pop();
-  let controller = url.slice(1).join("/");
-  let api = require("../api/" + controller);
-  let result = await api[method](req.params);
-  res.end(JSON.stringify(result));
+  let controller = url[1];
+  let method = url[2];
+  if (controller && method) {
+    try {
+      let api = require("../api/" + controller);
+      let result = await api[method](req.params);
+      res.end(JSON.stringify(result));
+    } catch (err) {
+      res.end(JSON.stringify({result: 0, message: err.code}))
+    }
+  } else
+    res.end(JSON.stringify({result: 0, message: 'Unresolved call'}))
 };
