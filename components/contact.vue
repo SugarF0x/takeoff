cd5dfea4d949d8077c62200fae369ebbfd7fa190
Sugar_F0x <malion.torchwood@yandex.ru>
2020-08-06 20:06:25 +0300

    :recycle: Move contact into its own component

---
+++ components/contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="ma-3"
+  >
+    <v-img
+      height="200px"
+      :src="user.avatar"
+    />
+    <v-card-title>{{ user.full_name }}</v-card-title>
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio repudiandae sint voluptate? Aperiam consequatur culpa cupiditate illum repudiandae saepe voluptas.
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "contact",
+    props: {
+      user: Object
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,23 +15,17 @@
              v-for="user in users"
              :key="user.id"
       >
-        <v-card class="ma-3"
-        >
-          <v-img
-            height="200px"
-            :src="user.avatar"
-          />
-          <v-card-title>{{ user.full_name }}</v-card-title>
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio repudiandae sint voluptate? Aperiam consequatur culpa cupiditate illum repudiandae saepe voluptas.
-          </v-card-text>
-        </v-card>
+        <contact :user="user"></contact>
       </v-col>
     </v-row>
   </v-layout>
 </template>
 
 <script>
+  /*
+    TODO: На странице контактов должна быть возможность добавлять/удалять/редактировать контакты,
+     также желательно наличие функции поиска.
+   */
   export default {
     middleware: ['auth'],
     async asyncData(context) {
